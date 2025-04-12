28335496b9a746df7fac3a697a27f68c91b4f5a4
ihomziak <ivan.homziak@onedatascan.com>
2025-04-12 23:01:18 +0200

    ref

--- mysql-init/tms.sql
+++ mysql-init/tms.sql
@@ -5,20 +5,23 @@ CREATE DATABASE IF NOT EXISTS transactiondatabase;
 USE transactiondatabase;
 
 -- Create the `transaction` table
-CREATE TABLE IF NOT EXISTS transaction (
-                                           transaction_id BIGINT(20) AUTO_INCREMENT PRIMARY KEY,     -- Primary key for transaction
-                                           transaction_uuid VARCHAR(255) NOT NULL,                   -- UUID of the transaction
-                                           sender_uuid VARCHAR(255) NOT NULL,                        -- UUID of the sender
-                                           receiver_uuid VARCHAR(255) NOT NULL,                      -- UUID of the receiver
-                                           amount DOUBLE NOT NULL,                                   -- Amount of transaction
-                                           transaction_status ENUM('NEW', 'CREATED', 'STARTED', 'COMPLETED', 'CANCELED', 'FAILED') NOT NULL,  -- Enum for transaction status
-                                           transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,     -- Creation timestamp
-                                           last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  -- Last update timestamp
-);
+CREATE TABLE IF NOT EXISTS transactions (
+                                            transaction_id BIGINT(20) AUTO_INCREMENT PRIMARY KEY,     -- Primary key for transaction
+    transaction_uuid VARCHAR(255) NOT NULL,                   -- UUID of the transaction
+    sender_uuid VARCHAR(255) NOT NULL,                        -- UUID of the sender
+    receiver_uuid VARCHAR(255) NOT NULL,                      -- UUID of the receiver
+    amount DOUBLE NOT NULL,                                   -- Amount of transaction
+    transaction_status ENUM('NEW', 'CREATED', 'STARTED', 'COMPLETED', 'CANCELED', 'FAILED') NOT NULL,  -- Enum for transaction status
+    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,     -- Creation timestamp
+    last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  -- Last update timestamp
+    );
 
 -- Example insert statements (optional) for testing purposes
-INSERT INTO transaction (transaction_uuid, sender_uuid, receiver_uuid, amount, transaction_status)
+INSERT INTO transactions (transaction_uuid, sender_uuid, receiver_uuid, amount, transaction_status)
 VALUES ('123e4567-e89b-12d3-a456-426614174001', '123e4567-e89b-12d3-a456-23423423', '3840ade4-f8ad-4829-a7af-b88192d52243', 100.00, 'NEW');
 
-INSERT INTO transaction (transaction_uuid, sender_uuid, receiver_uuid, amount, transaction_status)
+INSERT INTO transactions (transaction_uuid, sender_uuid, receiver_uuid, amount, transaction_status)
 VALUES ('123e4567-e89b-12d3-a456-426614174000', '123e4567-e89b-12d3-a456-42661417qweqwe', '3840ade4-f8ad-4829-a7af-b88192d52243', 50.00, 'COMPLETED');
+
+CREATE INDEX idx_transaction_uuid ON transactions(transaction_uuid);
+CREATE INDEX idx_transaction_status ON transactions(transaction_status);
